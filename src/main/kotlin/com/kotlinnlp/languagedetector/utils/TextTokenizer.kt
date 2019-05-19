6501573d9c0d7b5a6d866ff0bdc32ee110332c2b
KotlinNLP Authors <github@kotlinnlp.com>
2019-05-19 17:58:35 +0200

    Fix resources reading: read them as stream to make them working in a jar

--- src/main/kotlin/com/kotlinnlp/languagedetector/utils/TextTokenizer.kt
+++ src/main/kotlin/com/kotlinnlp/languagedetector/utils/TextTokenizer.kt
@@ -9,6 +9,7 @@ package com.kotlinnlp.languagedetector.utils
 
 import com.kotlinnlp.neuraltokenizer.NeuralTokenizer
 import com.kotlinnlp.neuraltokenizer.NeuralTokenizerModel
+import java.io.File
 
 /**
  * A simple tokenizer which splits a text by spacing and pointing chars.
@@ -39,7 +40,7 @@ class TextTokenizer(cjkModel: NeuralTokenizerModel) {
    * A set of the most frequent Chinese, Japanese and Korean characters.
    */
   private val cjkChars: Set<Char> = setOf(
-    *this::class.java.getResource("/CJKChars.txt").readText()
+    *this.javaClass.getResourceAsStream(File.separator + "CJKChars.txt").reader().readText()
       .split("\n")
       .filter { it.isNotEmpty() }
       .map { it[0] }
